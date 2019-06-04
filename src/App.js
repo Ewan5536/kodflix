b259d6f5bc81680cd6a9e95ae2f1f5ccea94c16e
Ewan Thomas <ewan5536@gmail.com>
2019-06-04 10:00:30 +0100

    add movie cover

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import cover from './movieCover.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Ewan.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <img src={cover} alt='movieCover'/>
+      <p>
+        <h1>Hello Ewan.</h1>
+      </p>
     </div>
   );
 }
