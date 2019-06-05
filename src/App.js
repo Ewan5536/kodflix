762583125790253b27745bc52d07a5139e9b50b5
Ewan Thomas <ewan5536@gmail.com>
2019-06-05 11:04:14 +0100

    #3 add 6 tv show titles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import cover from './movieCover.jpg';
 import './App.css';
+import './'
+import './'
+import './'
+import './'
+import './'
+import './'
 
 function App() {
   return (
-    <div className="App">
-    <img src={cover} alt='movieCover'/>
-      <p>
-        <h1>Hello Ewan.</h1>
-      </p>
-    </div>
+  <div className="App">
+    <div className="Row1">
+      <div className="TvTile"><h1>black mirror</h1></div>    
+      <div className="TvTile"><h1>breaking bad</h1></div>
+      <div className="TvTile"><h1>death note</h1></div>  
+    </div>   
+    <div className="Row2">
+      <div className="TvTile"><h1>game of thrones</h1></div>
+      <div className="TvTile"><h1>the walking dead</h1></div>  
+      <div className="TvTile"><h1>the wire</h1></div>   
+    </div> 
+  </div>
   );
 }
 
