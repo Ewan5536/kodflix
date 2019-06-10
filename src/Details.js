d4a0509bd4333fb44e177105b6831f96b1d022b5
Ewan Thomas <ewan5536@gmail.com>
2019-06-10 15:48:09 +0100

    #8 prt2 navi back to home

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 export default function Details(){
     return(
       <div>
         <h1>welcome to details page, WIP :)</h1>
+        <Link to="/">Back to home page</Link>
       </div>
     );
   }
--- src/ShowStack.js
+++ src/ShowStack.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 export default function ShowStack(props) {
     return( 
-    <div className="TvTile">
+    <Link to="/details" className="TvTile">
       <img src={props.tvShowLogo} alt={`${props.name} tvShowLogo`} />
       <div className="TitleOverLay">
         <h1>{props.name}</h1>
       </div>
-    </div>
+    </Link>
     );
   }
