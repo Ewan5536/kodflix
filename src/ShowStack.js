6f59c6c9b9b626c6922b7e8f83023f2034797dcd
Ewan Thomas <ewan5536@gmail.com>
2019-06-14 15:26:56 +0100

    #10 create unique details page url for each tv show

--- src/ShowStack.js
+++ src/ShowStack.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 export default function ShowStack(props) {
     return( 
-    <Link to="/details" className="TvTile">
+    <Link to={`/${props.id}`} className="TvTile">
       <img src={props.tvShowLogo} alt={`${props.name} tvShowLogo`} />
       <div className="TitleOverLay">
         <h1>{props.name}</h1>
