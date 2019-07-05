efb48bcc3072ba8cfb694494e472e1ad19203f2d
ewan thomas <ewan5536@gmail.com>
2019-07-05 15:01:42 +0100

    #19 crt list of tv shows. #20  improve backend.

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,8 @@
+const shows = require('./shows.js')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/rest/shows', (req, res) => res.send(shows()))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
