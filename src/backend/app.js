baa915ea0eee6a34238a7bbce5a3a9ff611db9e9
ewan thomas <ewan5536@gmail.com>
2019-07-15 17:18:41 +0100

    require node path middleware, edit scripts

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,9 @@
 const shows = require('./shows.js')
 const express = require('express')
 const app = express()
+const path = require('path');
 const port = process.env.PORT || 3001;
+
 
 app.get('/rest/shows', (req, res) => res.send(shows()))
 app.use(express.static(path.join(__dirname, '../../build')));
