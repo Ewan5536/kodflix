f8e81febc34f0f44c83d5d98c1773bc29d75477f
ewan thomas <ewan5536@gmail.com>
2019-07-22 15:03:15 +0100

    #24  Replace the TV show frontend , with  backend

--- src/frontend/Details/Details.js
+++ src/frontend/Details/Details.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import getTvshow from '../galTvShow';
+// import getTvshow from '../galTvShow';
 import './Details.css';
 
 export default class Details extends Component {
@@ -12,24 +12,22 @@ export default class Details extends Component {
     };
   }
   componentDidMount() {
-    let tvShow = this.props.match.params.showId;
-    let Show = getTvshow().find((Show) =>
-      Show.id === tvShow
-    );
-    this.setState({ Show });
-    fetch('/rest/shows').then(function(response) {
-      return response.json();
-    })
-    .then(function(getTvshow) {
-      console.log(JSON.stringify(getTvshow));
-    });
+    fetch ('/rest/shows').then(function(response) {
+      return response.json();})
+      .then(ShowsBackend => {
+        let tvShow = this.props.match.params.showId;
+        let Show = ShowsBackend.find((Show) =>
+        Show.id === tvShow
+      );
+    this.setState({ Show  });
+      })
   }
 
 
   render() {
     if (this.state.Show === undefined) {
       return <Redirect to='/not-found' />;
-    } else {
+    } else if (this.state.Show.id){
       return (
         <div className='Details'>
           <h1>{this.state.Show.name}</h1>
@@ -37,15 +35,14 @@ export default class Details extends Component {
             <div className= 'text'>
               <h2>{this.state.Show.synopsis}</h2>
             </div>
-            
-            <img
-              className='image'
-              src={this.state.Show.tvShowLogo}
-              alt={this.state.Show.name} />
+            <img src={require(`../${this.state.Show.id}.jpg`)} alt={`${this.state.Show.name} tvShowLogo`} />
           </div>
           <Link to="/"><h2><i>Back to home page</i></h2></Link>
         </div>
       );
+    }
+    else {
+      return <div/>
     }
   }
 }
--- src/frontend/ShowStack.js
+++ src/frontend/ShowStack.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function ShowStack(props) {
     return( 
     <Link to={`/${props.id}`} className="TvTile">
-      <img src={props.tvShowLogo} alt={`${props.name} tvShowLogo`} />
+      <img src={require(`./${props.id}.jpg`)} alt={`${props.name} tvShowLogo`} />
       <div className="TitleOverLay">
         <h1>{props.name}</h1>
       </div>
